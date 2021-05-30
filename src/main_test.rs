84d7676807245f3719b9b778a0ce75b58b2fa0a5
Dustin Blackman <dev@dustinblackman.com>
2021-05-30 17:35:44 -0400

    add ci

--- src/main_test.rs
+++ src/main_test.rs
@@ -5,9 +5,9 @@ use speculate::speculate;
 speculate! {
     describe "get_pkg_version" {
         it "should return a name and version number" {
-            let res = get_pkg_version("cargo-watch").unwrap();
-            expect!(res.name).to(be_equal_to("cargo-watch"));
-            expect!(res.version).to(be_equal_to("7.8.0"));
+            let res = get_pkg_version("cargo-cmd").unwrap();
+            expect!(res.name).to(be_equal_to("cargo-cmd"));
+            expect!(res.version).to(be_equal_to("0.3.1"));
         }
 
         it "should return an error when a package is not found" {
