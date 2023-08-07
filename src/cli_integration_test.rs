9a940f7dee59fca6136d4f82a39b141fae95f580
Dustin Blackman <dev@dustinblackman.com>
2023-08-07 16:14:51 -0400

    fix: integration tests with code coverage commands

--- src/cli_integration_test.rs
+++ src/cli_integration_test.rs
@@ -1,8 +1,13 @@
 use assert_cmd::Command;
+use crate::metadata;
+
+fn get_bin() -> String {
+    return metadata::get_project_root().unwrap().join("target/debug/cargo-bin").to_str().unwrap().to_string();
+}
 
 #[test]
 fn it_syncs_aliases_successfully() {
-    let mut cmd = Command::cargo_bin("cargo-bin").unwrap();
+    let mut cmd = Command::cargo_bin(get_bin()).unwrap();
     let assert = cmd.arg("--sync-aliases").assert();
 
     assert.success();
@@ -10,7 +15,7 @@ fn it_syncs_aliases_successfully() {
 
 #[test]
 fn it_builds_all_bins_successfully() {
-    let mut cmd = Command::cargo_bin("cargo-bin").unwrap();
+    let mut cmd = Command::cargo_bin(get_bin()).unwrap();
     let assert = cmd.arg("--build").assert();
 
     assert.success();
@@ -18,7 +23,7 @@ fn it_builds_all_bins_successfully() {
 
 #[test]
 fn it_runs_nextest_help_successfully() {
-    let mut cmd = Command::cargo_bin("cargo-bin").unwrap();
+    let mut cmd = Command::cargo_bin(get_bin()).unwrap();
     let assert = cmd.arg("cargo-nextest").arg("--help").assert();
 
     assert.success();
@@ -26,7 +31,7 @@ fn it_runs_nextest_help_successfully() {
 
 #[test]
 fn it_runs_nextest_help_from_cargo_successfully() {
-    let mut cmd = Command::cargo_bin("cargo-bin").unwrap();
+    let mut cmd = Command::cargo_bin(get_bin()).unwrap();
     let assert = cmd.arg("bin").arg("cargo-nextest").arg("--help").assert();
 
     assert.success();
@@ -34,7 +39,7 @@ fn it_runs_nextest_help_from_cargo_successfully() {
 
 #[test]
 fn it_runs_help_successfully() {
-    let mut cmd = Command::cargo_bin("cargo-bin").unwrap();
+    let mut cmd = Command::cargo_bin(get_bin()).unwrap();
     let assert = cmd.arg("--help").assert();
 
     assert.success();
